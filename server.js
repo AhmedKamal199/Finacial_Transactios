8f622b8943cf5bf39d5c33e618d2ff329d865782
AhmedKamal199 <akm.kamal123456@gmail.com>
2022-04-20 16:45:26 +0200

    static

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-const express =require("express");
+const express = require("express");
 const app = express();
+const cors = require("cors");
 
-app.use(express.json())
-app.get("/",(req,res)=>{
-	res.send("hello")
+app.use(express.static("./client"));
+app.use(express.json());
+app.use(cors("*"));
+
+app.get("/", (req, res) => {
+  const name = "akm";
+  res.send("hello");
 });
+
 // DB connections
 require("./Db_con/connection");
 // require("./controllers/testDB")();
 
-const Wallet = require('./models/Wallets');
+const Wallet = require("./models/Wallets");
 
-const port = 5500;
+const port = 5000;
 
-app.use('/api',require("./routes/main"))
+app.use("/api", require("./routes/main"));
 
-async function run(){
-	await sequelize.sync({force:true}).then(()=> console.log("conneted"));
-	app.listen(port, console.log(`Server is run on port ${port}`))
+async function run() {
+  await sequelize.sync({ force: true }).then(() => console.log("conneted"));
+  app.listen(port, console.log(`Server is run on port ${port}`));
 }
 
 run();
