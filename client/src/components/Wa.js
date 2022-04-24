37a4ae3b76e92c0b8114fadc23241381d0add745
AhmedKamal199 <akm.kamal123456@gmail.com>
2022-04-24 13:50:37 +0200

    Get Wallets

--- client/src/components/Wa.js
+++ client/src/components/Wa.js
@@ -2,23 +2,27 @@ import React, { useState, useEffect } from "react";
 import url from "../config";
 import axios from "axios";
 const Wa = () => {
-  const [name, Setname] = useState("");
+  // const [name, Setname] = useState("");
   const [load, reload] = useState(false);
   const [wa, setWallet] = useState([]);
   useEffect(async () => {
     const { data } = await axios.get(`${url}/api/wallets`);
-    console.log(data.wallets);
     const Wallets = data.wallets;
+    setWallet([...Wallets]);
     console.log(Wallets);
-  }, []);
+  }, [wa.length]);
+  console.log(wa);
   return (
     <div className="wa-con">
       <div className="contanier">
         <h2 className="sub">Wallets</h2>
+        <button>Add Wallet</button>
         <div className="items">
-          <div className="item">
-            <h1 className="test"></h1>
-          </div>
+          {wa.map(({ id, name, amount }) => (
+            <div className="item" key={id}>
+              <h1 className="test"> {name} </h1>
+            </div>
+          ))}
         </div>
       </div>
     </div>
