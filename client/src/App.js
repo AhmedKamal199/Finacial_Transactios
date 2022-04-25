23b2f0426193a11b8605b320c42f5d5025396ed6
AhmedKamal199 <akm.kamal123456@gmail.com>
2022-04-25 16:27:08 +0200

    Modify Navbar with bugs

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import AppNavbar from "./components/AppNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "reactstrap";
 import Wa from "./components/Wa";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <AppNavbar />
       <Wa />
+      <Footer />
       {/* <Container>
           <ItemModal />
           <ShoppingList />
--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,36 +1,76 @@
 import React from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 const AppNavbar = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <h2 className="title">
-              {/* <i className="fa-solid fa-wallet"></i> */}
-              Fin
-              {/* <!--acial Transactions --> */}
-            </h2>
-          </li>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Transactions</a>
-          </li>
-          <li className="sig">
-            <a href="#">About</a>
-          </li>
-          <li className="reg">
-            <button className="btn g">Register</button>
-          </li>
-          <li>
-            <button className="btn w">Sign in</button>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <i
+              className="bi bi-wallet mx-3 bg-"
+              style="font-size: 1.5rem; color: white;"
+            ></i>
+            Finacial Transactions
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navmenu"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="navbar-collapse collapse" id="navmenu">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  Transactions
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#about" className="nav-link">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
   );
 };
 
 export default AppNavbar;
+
+{
+  /* <nav>
+  <ul>
+    <li>
+      <h2 className="title">
+        {/* <i className="fa-solid fa-wallet"></i> *
+        
+      </h2>
+    </li>
+    <li>
+      <a href="#">Home</a>
+    </li>
+    <li>
+      <a href="#">Transactions</a>
+    </li>
+    <li className="sig">
+      <a href="#">About</a>
+    </li>
+    <li className="reg">
+      <button className="btn g">Register</button>
+    </li>
+    <li>
+      <button className="btn w">Sign in</button>
+    </li>
+  </ul>
+</nav> */
+}
